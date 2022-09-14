494ff6f3e7f2943f4a6e711647f03365a2721941
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2022-09-14 13:38:14 -0400

    updated the site with refactored code

--- base-apparel-coming-soon/js/script.js
+++ base-apparel-coming-soon/js/script.js
@@ -1,20 +1,10 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
+// const input = document.querySelector('input')
+// const errorIcon = document.querySelector('.error-icon')
+// const small = document.querySelector('small')
+const errElements = document.querySelectorAll('.err-test')
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	const emailVal = email.value;
+// const allElements = [input, errorIcon, small]
 
-	// check if it is a valid email
-	if (!validateEmail(emailVal)) {
-		form.classList.add('error');
-	} else {
-		form.classList.remove('error');
-		document.body.innerHTML = `<div style="display:flex; align-items: center; justify-content: center;"><h1>Thank you!</h1></div>`;
-	}
-});
-
-function validateEmail(email) {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
-}
+errElements.forEach((e) => {
+    e.classList.add('error')
+})
